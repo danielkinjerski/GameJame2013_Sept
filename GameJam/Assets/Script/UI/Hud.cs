50d0cbd20db5f73f23d12f7ad844c3c5c2d68a93
Daniel kinjerski <danielkinjerski@aol.com>
2013-09-21 21:47:08 -0400

    timer added

---
+++ GameJam/Assets/Script/UI/Hud.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hud : MonoBehaviour {
+
+    public GameManager gm;
+    public UILabel time, whiteBlood, virusCells;
+
+	// Use this for initialization
+	void Start () {
+        gm.onTimerIncrement += gm_onTimerIncrement;
+	}
+
+    void gm_onTimerIncrement(string text)
+    {
+        time.text = text;
+    }
+}
