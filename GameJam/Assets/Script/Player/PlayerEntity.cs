495749157572985d5faf561be0d9ed804c95ceb5
JacobCorle <jacobcorle@gmail.com>
2013-09-22 03:18:08 -0400

    k

--- GameJam/Assets/Script/Player/PlayerEntity.cs
+++ GameJam/Assets/Script/Player/PlayerEntity.cs
@@ -7,7 +7,7 @@ public class PlayerEntity : BaseEntity
 	
     public override Vector2 InputMovement()
     {
-        Vector3 dir = Camera.main.transform.forward * Input.GetAxis("Vertical") + Input.GetAxis("Horizontal") * Camera.main.transform.right;
+        Vector3 dir = Camera.main.transform.up * Input.GetAxis("Vertical") + Input.GetAxis("Horizontal") * Camera.main.transform.right;
         Vector2 dir2 = new Vector2(dir.x, dir.z);
         return dir2.normalized;
     }
