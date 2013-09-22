561d82ec0a18e6489d4b4c3f3115ac721b8eb487
Daniel kinjerski <danielkinjerski@aol.com>
2013-09-21 21:01:27 -0400

    wut is this i don't even

--- GameJam/Assets/Script/GameManager/GameManager.cs
+++ GameJam/Assets/Script/GameManager/GameManager.cs
@@ -21,83 +21,35 @@ public class GameManager : MonoBehaviour
 
     #region Variables
 
-    public GameObject OpeningWindow, GameOverWindow, SelectionWindow, TutorialWindow,
-                    BlackWorld, WhiteWorld,
-                    BlackCam, WhiteCam, MainCam,
-                    Facebook, fbbutton, fbsuccess, fbpost,
-                    Character,
-                    CharBRender, CharWRender;
-    UILabel fb;
-    public UILabel deathLbl, timeLbl;
-    public Material BlackMat, WhiteMat;
-    public static GameState gameState = GameState.OpeningWindow;
-    public static CurrentPlayMode currentPlayMode = CurrentPlayMode.Black;
-    private bool toggle;
-    public bool cheats;
-    private int deaths;
-    private float time, maxTime;
+    public float levelTimer = 0, maxTime;
 
     #endregion
 
-    //#region Mono Inherit Functions
-
-    //void Awake () {
-    //    BlackMat.color = new Color(BlackMat.color.r, BlackMat.color.g, BlackMat.color.b, 1f);
-    //    WhiteMat.color = new Color(WhiteMat.color.r, WhiteMat.color.g, WhiteMat.color.b, 1f);
-    //    gameState = GameState.OpeningWindow;
-    //    OpeningWindow.SetActiveRecursively(true);
-    //    GameOverWindow.SetActiveRecursively(false);
-    //    SelectionWindow.SetActiveRecursively(false);
-    //    TutorialWindow.SetActiveRecursively(false);
-        
-
-    //    BlackCam.camera.rect = new Rect(0.5f, 0, 0.5f, 1);
-    //    WhiteCam.camera.rect = new Rect(0, 0, 0.5f, 1);
-
-    //    MainCam.camera.rect = new Rect(0, 0, 1, 1);
-    //    MainCam.active = Facebook.active = true;
-    //    BlackCam.active = WhiteCam.active = fbsuccess.active = false;
-    //    fb = fbsuccess.GetComponent<UILabel>();
-
-    //    deathLbl.text = "You died: @ time(s)!";
-    //    timeLbl.text = "Your max time: @";
-    //}
-	
-    //void Update ()
-    //{
-
-    //    #region Fade in/out
-    //    if (toggle)
-    //    {
-    //        if (GameManager.currentPlayMode == CurrentPlayMode.Black)
-    //        {
-    //            if (Toggle(ref WhiteMat, true)&&Toggle(ref BlackMat, false))
-    //            {
-    //                toggle = false;
-    //                ActivateBlackMode(false);
-    //                GameManager.currentPlayMode = CurrentPlayMode.White;
-    //            }
-    //        }
-    //        else if (GameManager.currentPlayMode == CurrentPlayMode.White)
-    //        {
-    //            if(Toggle(ref BlackMat, true)&& Toggle(ref WhiteMat, false))
-    //            {
-    //                toggle = false;
-    //                ActivateWhiteMode(false);
-    //                GameManager.currentPlayMode = CurrentPlayMode.Black;
-    //            }
-    //        }
-    //    }
-    //    #endregion
-    //}
-
-    //#endregion
+    
 
     #region UI Events
 
     void PlayHit()
     {
         Debug.Log("Play");
+        StartCoroutine(GameTimer());
+    }
+
+    IEnumerator GameTimer()
+    {
+        levelTimer = Time.time;
+        while (Time.time < levelTimer + maxTime)
+        {
+            print(Time.time);
+            yield return null;
+        }
+        print("time Up");
+        yield break;
+    }
+
+    void Update()
+    {
+
     }
 
     #endregion
