7974781b84c636dcbfd4752923ab67f17decc676
JacobCorle <jacobcorle@gmail.com>
2013-09-21 20:40:31 -0400

    camera fixed

--- GameJam/Assets/Standard Assets/MouseOrbit.js
+++ GameJam/Assets/Standard Assets/MouseOrbit.js
@@ -6,11 +6,11 @@ var distance = 10.0;
 var xSpeed = 250.0;
 var ySpeed = 120.0;
 
-var yMinLimit = -20;
-var yMaxLimit = 80;
+/*var yMinLimit = -20;
+var yMaxLimit = 80;*/
 
 private var x = 0.0;
-private var y = 0.0;
+//private var y = 0.0;
 
 @AddComponentMenu("Camera-Control/Mouse Orbit")
 partial class MouseOrbit { }
@@ -18,7 +18,7 @@ partial class MouseOrbit { }
 function Start () {
     var angles = transform.eulerAngles;
     x = angles.y;
-    y = angles.x;
+    //y = angles.x;
 
 	// Make the rigid body not change rotation
    	if (rigidbody)
@@ -28,11 +28,11 @@ function Start () {
 function LateUpdate () {
    
     		x += Time.deltaTime * Input.GetAxis("Mouse X") * xSpeed;
-    		y -= Time.deltaTime * Input.GetAxis("Mouse Y") * ySpeed;
-    		y = ClampAngle(y, yMinLimit, yMaxLimit);
+    		//y -= Time.deltaTime * Input.GetAxis("Mouse Y") * ySpeed;
+    		//y = ClampAngle(y, yMinLimit, yMaxLimit);
     	
 
-        var rotation = Quaternion.EulerAngles(y * Mathf.Deg2Rad, x * Mathf.Deg2Rad, 0);
+        var rotation = Quaternion.EulerAngles(/*y * Mathf.Deg2Rad*/35 * Mathf.Deg2Rad, x * Mathf.Deg2Rad, 0);
         var position = rotation * Vector3(0.0, 0.0, -distance) + target.position;
         
         transform.rotation = rotation;
